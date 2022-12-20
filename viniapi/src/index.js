792483af65b65f2a12f665743afdb55043237e0c
Max <maxime.vollat@epitech.eu>
2022-12-20 16:50:30 +0000

    ADD: updated and fixed code but out of time

--- viniapi/src/index.js
+++ viniapi/src/index.js
@@ -1,5 +1,3 @@
-// ./src/index.js
-// importing the dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -7,20 +5,14 @@ const fetch = require('node-fetch');
 
 const temp_url = "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f6673324-9e0d-4071-8a63-c5fadab5efaa/wines_admin.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221220%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221220T130953Z&X-Amz-Expires=86400&X-Amz-Signature=979666e529d8d53fe2146f3a83961761e1f8d31435ac8a3bd3cbb49ba7041d1b&X-Amz-SignedHeaders=host&x-id=GetObject"
 
-// defining the Express app
 const app = express();
 
-require('./routes/wine')(app);
-
-// defining an array to work as the database (temporary solution)
-
-// using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
 
-// enabling CORS for all requests
 app.use(cors());
 
 var wines = []
+
 
 async function initWinesDB() {
   try {
@@ -37,8 +29,23 @@ async function initWinesDB() {
 } 
 
 initWinesDB();
-  
-// starting the server
+
+app.get('/api/wine', (req, res) => {
+  const name = req.query.name.toLowerCase();
+  if (!name)
+    return res.send('No name provided');
+  if (!wines)
+    return res.send('No wines loaded');
+  const found_wines = wines.filter(wine =>
+    wine.name.toLowerCase().includes(name) ||
+    wine.appellation.toLowerCase().includes(name) ||
+    wine.grapeVarieties.toLowerCase().includes(name) ||
+    wine.region.toLowerCase().includes(name)
+  );
+  console.log(found_wines?.length);
+  res.send(found_wines);
+})
+
 app.listen(8080, () => {
   console.log('listening on port 8080');
 });
